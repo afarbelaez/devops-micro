7f424e36d355d91265576c40300ee8066925070e
Andres Felipe Arbelaez Rios <afar182@example.com>
2026-05-07 09:42:27 -0500

    test: forzar fallo CI para documentacion entrega 3

--- tests/test_blacklist.py
+++ tests/test_blacklist.py
@@ -20,7 +20,7 @@ class TestPostBlacklist:
             content_type='application/json',
             headers=auth_headers
         )
-        assert response.status_code == 201
+        assert response.status_code == 999  # FORZAR FALLO CI
         data = response.get_json()
         assert 'message' in data
         assert sample_payload['email'] in data['message']
@@ -100,7 +100,7 @@ class TestPostBlacklist:
             content_type='application/json',
             headers=auth_headers
         )
-        assert response.status_code == 201
+        assert response.status_code == 999  # FORZAR FALLO CI
 
 
 # ---------------------------------------------------------------------------
